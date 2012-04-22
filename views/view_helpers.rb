06528940398e3da9fc2c07fe8e9c4912e71aa575
Matthew Anderson <manderson@onehub.com>
2012-04-22 16:12:19 -0700

    Playing with some different backgrounds.

--- views/view_helpers.rb
+++ views/view_helpers.rb
@@ -19,7 +19,58 @@ module ViewHelpers
   def hidden(&block)
     #no-op
   end
+end
+
+module ContentHelpers
+  def content_for(symbol, content = nil, &block)
+    content = capture(&block) if block_given?
+    set_content_for(symbol, content) if content
+    get_content_for(symbol) unless content
+  end
   
-  # Add your own helpers below...
+  def content_for?(symbol)
+    !(get_content_for(symbol)).nil?
+  end
   
+  def get_content_for(symbol = :content)
+    if symbol.to_s.intern == :content
+      @content
+    else
+      instance_variable_get("@content_for_#{symbol}")
+    end
+  end
+  
+  def set_content_for(symbol, value)
+    instance_variable_set("@content_for_#{symbol}", value)
+  end
+  
+  def capture_erb(&block)
+    buffer = ""
+    old_buffer, @_out_buf = @_out_buf, buffer
+    yield
+    buffer
+  ensure
+    @_out_buf = old_buffer
+  end
+  alias capture_rhtml capture_erb
+  alias capture_erubis capture_erb
+  
+  def capture(&block)
+    capture_method = "capture_#{script_extension}"
+    if respond_to? capture_method
+      send capture_method, &block
+    else
+      raise "Capture not supported for `#{script_extension}' template (#{engine_name})"
+    end
+  end
+  
+  private
+    
+    def engine_name
+      Tilt[script_extension].to_s
+    end
+    
+    def script_extension
+      parser.script_extension
+    end
 end
